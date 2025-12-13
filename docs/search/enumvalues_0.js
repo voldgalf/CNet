6d317e8a6589d5ffc20df8a02ebbe9e52362dc4e
brick <50calibursword@gmail.com>
2025-12-13 06:55:24 -0500

    Updated CMakeLists.txt

--- docs/search/enumvalues_0.js
+++ docs/search/enumvalues_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['incorrect_5fsocket_5ftype_0',['INCORRECT_SOCKET_TYPE',['../globals_8h.html#a2ef8c26e904f3db1c7c69221c7f795fba3707c0f405cae0f5beebb548f3ac0f2f',1,'globals.h']]]
+  ['incorrect_5fsocket_5ftype_0',['INCORRECT_SOCKET_TYPE',['../globals_8h.html#a38ac89f40d0c83b4849fdc034001a86ba3707c0f405cae0f5beebb548f3ac0f2f',1,'globals.h']]]
 ];
