e31d91ebaaa3ff6fac83108e871a8c3752fa0299
brick <50calibursword@gmail.com>
2025-12-13 07:08:06 -0500

    Replaced 'nullptr' with NULL

--- include/globals.h
+++ include/globals.h
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #ifdef  __WIN32
 #include <winsock2.h>
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #include "include/globals.h"
 
